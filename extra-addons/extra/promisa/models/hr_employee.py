b37c100d461ddb2523ed087b4ef95b82041c3672
Mirsha Rey <mrc@indboo.com>
2021-09-13 13:07:08 -0600

    [ADD] models import in Promisa module and modifications in employee view

--- extra-addons/extra/promisa/models/hr_employee.py
+++ extra-addons/extra/promisa/models/hr_employee.py
@@ -5,5 +5,5 @@ from odoo import fields, models, api
 class addressFields(models.Model):
     _inherit = 'hr.employee'
 
-    address_char = fields.Char(string="Dirección")
+    address_char = fields.Text(string="Dirección")
     locker_no = fields.Char(string="No. Locker")
